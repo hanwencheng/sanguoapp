1614eb8c4ff78f4b536d012edc7ec08e46269a9c
Hanwen <heawen.cheng@gmail.com>
2014-10-21 15:41:58 +0200

    change html to js dynamic write

--- js/hero/abilities.js
+++ js/hero/abilities.js
@@ -8,11 +8,43 @@ define(['require','text!hero/abilities.html'],function(require, html){
 
     };
 
-    container.innerHTML = html;
-    var abilities = [];
-    abilities = ['rider', 'cool', 'strong', 'quiet'];
+//    container.innerHTML = html;
 
-    abilities.forEach(function(ability){
-        console.log('good');
+    var abilities = ['rider', 'cool', 'strong', 'quiet',
+        4,5,60,70,80,90,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29];
+    console.log('abilities array is' + abilities);
+//    abilities = ;
+
+    var rowElements = 6, rowContainer;
+    abilities.forEach(function(ability, index){
+        if(index%rowElements === 0){
+            rowContainer = document.createElement('div');
+            rowContainer.id = 'ability-' + (index/rowElements + 1);
+            container.appendChild(rowContainer);
+        }
+        var abilityContainer = document.createElement('div');
+        abilityContainer.className = 'btn btn-pull';
+        abilityContainer.id = index;
+        rowContainer.appendChild(abilityContainer);
+
+        var iconEl = document.createElement('div');
+        var textEl = document.createElement('div');
+        iconEl.className = 'icon';
+        console.log('test .class function' + iconEl.class);
+        textEl.className = 'btn-text';
+
+        var iconText="";
+        iconText += "                <span class=\"ability-up\">v<\/span>";
+        iconText += "                <span>a<\/span>";
+        iconText += "                <span class=\"ability-down\">n<\/span>";
+        iconEl.innerHTML = iconText;
+
+        var textNode = document.createTextNode(ability);
+        textEl.appendChild(textNode);
+
+        abilityContainer.appendChild(iconEl);
+        abilityContainer.appendChild(textEl);
     });
+
+
 });
